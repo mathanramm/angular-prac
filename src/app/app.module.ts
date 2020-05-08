9e1e38119a78ffe9a5d0cd760e4a3e0976aaacb9
Mathan Ram  Marikani <meetmemathanram@yahoo.com>
2020-05-08 09:12:38 +0530

    new component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { from } from 'rxjs';
+import { ServerComponent } from './server/server/server.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent
   ],
   imports: [
     BrowserModule,
